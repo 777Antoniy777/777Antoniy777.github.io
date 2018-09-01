1d7ad4541906e42614c60c502ee8dea3f80c38eb
777Antoniy777 <tat7106@yandex.ru>
2018-09-01 20:17:44 +0600

    можно отправлять на проверку

--- sites/Device/js/main.js
+++ sites/Device/js/main.js
@@ -1,26 +1,62 @@
 var feedback = document.querySelector(".feedback");
 var feedback_button = document.querySelector(".contacts-link a");
 var feedback_close = feedback.querySelector(".button-close");
+var form = feedback.querySelector("form");
+var login = feedback.querySelector(".feedback-name");
+var email = feedback.querySelector(".feedback-email");
 var map = document.querySelector(".map-popup");
 var map_button = document.querySelector(".map");
 var map_close = map.querySelector(".button-close");
 
+var support_storage = true;
+var storage_name = "";
+
+try {
+  storage_name = localStorage.getItem("login");
+}
+catch (err) {
+  support_storage = false;
+}
+
 feedback_button.addEventListener("click", function (evt) {
   evt.preventDefault();
   feedback.classList.add("feedback-show");
+  if (storage_name) {
+    storage_name = login.value;
+    email.focus();
+  }
+  else {
+    login.focus();
+  }
+});
+
+form.addEventListener("submit", function(evt) {
+  if (!login.value || !email.value) {
+    evt.preventDefault();
+    feedback.classList.remove("error-show");
+    feedback.offsetWidth = feedback.offsetWidth;
+    feedback.classList.add("error-show");
+  } else {
+
+    if (storage_name) {
+      localStorage.setItem("login", login.value);
+    }
+  }
 });
 
 feedback_close.addEventListener("click", function (evt) {
   evt.preventDefault();
   feedback.classList.remove("feedback-show");
+  feedback.classList.remove("error-show");
 });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
-    evt.preventDefault();
 
       if (feedback.classList.contains("feedback-show")) {
         feedback.classList.remove("feedback-show");
+        feedback.classList.remove("error-show");
+        evt.preventDefault();
       }
   }
 });
@@ -37,10 +73,10 @@ map_close.addEventListener("click", function (evt) {
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
-    evt.preventDefault();
       
       if (map.classList.contains("map-popup-show")) {
         map.classList.remove("map-popup-show");
+        evt.preventDefault();
       }
   }
 });
