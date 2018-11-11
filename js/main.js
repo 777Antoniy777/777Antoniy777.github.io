5d6d798574e6cda15015d6f05d9ced848b87b3ca
Антон <tat7106@gmail.com>
2018-11-11 13:27:25 +0600

    замена

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 var first_project = document.querySelector(".first-button");
 var second_project = document.querySelector(".second-button");
-var first_demands_button = document.querySelector(".demands-element-button");
+var third_project = document.querySelector(".third-button");
+var first_demands_button = document.querySelector(".first-demands-element-button");
+var second_demands_button = document.querySelector(".second-demands-element-button");
 var first_picture = document.querySelector(".first-picture");
 var second_picture = document.querySelector(".second-picture");
-var first_demands = document.querySelector(".demands-discription");
+var third_picture = document.querySelector(".third-picture");
+var first_demands = document.querySelector(".first-demands-discription");
+var second_demands = document.querySelector(".second-demands-discription");
 
 first_project.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -15,7 +19,17 @@ second_project.addEventListener("click", function (evt) {
   second_picture.classList.toggle("picture-show");
 });
 
+third_project.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  third_picture.classList.toggle("picture-show");
+});
+
 first_demands_button.addEventListener("click", function (evt) {
   evt.preventDefault();
   first_demands.classList.toggle("demands-discription-show");
+});
+
+second_demands_button.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  second_demands.classList.toggle("demands-discription-show");
 });
